8ba11e4592d50a61588f678e552a65fb85160a00
Leo <leoonorio_02@hotmail.com>
2020-02-05 17:15:05 -0500

    Inicio de la Pagina

---
+++ src/main/java/com/Leather/springboot/backend/apirest/KlausApplication.java
@@ -0,0 +1,13 @@
+package com.Leather.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlausApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Leather/springboot/backend/apirest/KlausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Leather.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
