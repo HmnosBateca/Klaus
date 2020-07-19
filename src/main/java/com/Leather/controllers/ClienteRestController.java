07cb2f5a787edb0e0bab35aac31ca0af2334edac
Leo <leoonorio_02@hotmail.com>
2020-07-19 15:50:26 -0500

    Se validacion del CRUD del backend para el Cliente

--- src/main/java/com/Leather/controllers/ClienteRestController.java
+++ src/main/java/com/Leather/controllers/ClienteRestController.java
@@ -1,11 +1,15 @@
 package com.Leather.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,62 +28,110 @@ import com.Leather.models.entity.Cliente;
 import com.Leather.models.services.ICiudadService;
 import com.Leather.models.services.IClienteService;
 
-
-
-@CrossOrigin(origins= {"http://localhost:4200", "*"})
-@RestController//api rest
-@RequestMapping("/api")//generamos la url
-
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
+@RestController // api rest
+@RequestMapping("/api") // generamos la url
 
 public class ClienteRestController {
 
-
 	@Autowired
-	private IClienteService clienteService;//listado de clientes
+	private IClienteService clienteService;// listado de clientes
+	private Map<String, Object> mapa;
 
-	@GetMapping("/clientes")//listar peticion Get url  generamos empoing de la peticion
-	public List<Cliente>index(){
-		return clienteService.findAll();//metodo
+	@GetMapping("/clientes") // listar peticion Get url generamos empoing de la peticion
+	public List<Cliente> index() {
+		return clienteService.findAll();// metodo
 	}
-	
 
 	@GetMapping("/clientes/pagina")
-	public Page<Cliente> index(Pageable pageable){
+	public Page<Cliente> index(Pageable pageable) {
 		return clienteService.findAll(pageable);
 	}
-	
-	
-	@GetMapping("/clientes/{id}")//Get por id
-	public Cliente show(@PathVariable Long id) {
-		return clienteService.findById(id);//metodo Get por id
+
+	@GetMapping("/clientes/{id}") // Get por id
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Cliente cliente = null;
+		Map<String, Object> mapa = new HashMap<>();
+		try {// si hay errror
+			cliente = clienteService.findById(id);
+		} catch (DataAccessException e) {
+			mapa.put("mensaje", "Error al realizar la consulta en base de datos");
+			mapa.put("error", e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.INTERNAL_SERVER_ERROR);// status 500
+		}
+		if (cliente == null) {
+			mapa.put("mensaje", "El cliente ID:".concat(id.toString().concat("No existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.NOT_FOUND);// status 404
+		}
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
 	}
-	
-	//Post Agregar Cliente
+
+	// Post Agregar Cliente
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente) {
-    return clienteService.save(cliente);
+	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+		Cliente clienteNew = null;
+		Map<String, Object> mapa = new HashMap<>();
+		try {
+			clienteNew = clienteService.save(cliente);
+		} catch (DataAccessException e) {
+			mapa.put("mensaje", "error al realiar insert en la base de datos");
+			mapa.put(" error", e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));// por que
+																											// ocurrio
+																											// el error
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		mapa.put("mensaje", "El cliente ha sido creado con éxito!");
+		mapa.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.CREATED);
+
 	}
- 
-	@PutMapping("/clientes/{id}")//con la id obtenemos de la base de datos y actualizamos 
+
+	@PutMapping("/clientes/{id}") // con la id obtenemos de la base de datos y actualizamos
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {//un cliente ya modificado
-		Cliente clienteActual=clienteService.findById(id);//obtenemos el ciente por id
-		clienteActual.setDocumento(cliente.getDocumento());
-		clienteActual.setNombres(cliente.getNombres());
-		clienteActual.setApellidos(cliente.getApellidos());
-		clienteActual.setNumero_contacto(cliente.getNumero_contacto());
-		clienteActual.setDireccion(cliente.getDireccion());
-		clienteActual.setCorreo(cliente.getCorreo());
-		clienteActual.setCodigo_postal(cliente.getCodigo_postal());	
-		clienteActual.setCiudad(cliente.getCiudad());
-		
-		return clienteService.save(clienteActual);
+	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {// un cliente ya modificado
+		Cliente clienteActual = null;
+		Map<String, Object> mapa = new HashMap<>();
+		clienteActual = clienteService.findById(id);// obtenemos el ciente por id
+		if (clienteActual == null) {
+			mapa.put("mensaje", "Error: no se puede editar, el cliente iD:"
+					.concat(id.toString().concat("no existe en la base de datos")));
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.NOT_FOUND);// Estatus 404
+		}
+		try {
+			clienteActual.setDocumento(cliente.getDocumento());
+			clienteActual.setNombres(cliente.getNombres());
+			clienteActual.setApellidos(cliente.getApellidos());
+			clienteActual.setNumero_contacto(cliente.getNumero_contacto());
+			clienteActual.setDireccion(cliente.getDireccion());
+			clienteActual.setCorreo(cliente.getCorreo());
+			clienteActual.setCodigo_postal(cliente.getCodigo_postal());
+			clienteActual.setCiudad(cliente.getCiudad());
+			clienteActual = clienteService.save(clienteActual);// persistir o guardar
+		} catch (DataAccessException e) {
+			mapa.put("mensaje", "Error al actualizar cliente en la base de datos");
+			mapa.put("error", e.getMessage().concat(e.getMostSpecificCause().getMessage()));// por que ocurrio el error
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.INTERNAL_SERVER_ERROR);// Staus
+		}
+		mapa.put("mensaje", "El cliente ha sido actualizado con éxitos!");
+		mapa.put("cliente", clienteActual);
+		return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/clientes/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id) {
-		clienteService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		Map<String, Object> mapa = new HashMap<>();
+		try {
+			clienteService.delete(id);
+		}catch(DataAccessException e) {
+			mapa.put("mensaje", "Error al eliminar de la base de datos");
+			mapa.put("error", e.getMessage().concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.INTERNAL_SERVER_ERROR);//status 404
+			
+		}
+		mapa.put("mensaje", "El cliente ha sido eliminado con éxito!");
+		return new ResponseEntity<Map<String, Object>>(mapa, HttpStatus.OK);
+		
 	}
 }
